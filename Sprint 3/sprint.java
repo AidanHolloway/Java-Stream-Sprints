43f1f547580b9e7dcc258e50bb4e418350eaf3a8
Aidan Holloway <interncptwex037@capaciti.org.za>
2020-06-18 17:59:59 +0200

    Update sprint.java
    Added Comments for readability
    
    Co-Authored-By: Michael Munnik <zaenzoza@users.noreply.github.com>

--- Sprint 3/sprint.java
+++ Sprint 3/sprint.java
@@ -6,6 +6,8 @@ public class sprint{
         Scanner input = new Scanner(System.in);
         
         int rows = input.nextInt();
+       
+       //If the user enters a value outside of the alphabetical range, the program is terminated
         if(rows<1 || rows>26){
             System.out.println("Invalid value entered. Exiting program.");
             System.exit(0);
@@ -19,12 +21,15 @@ public class sprint{
             String reply = input.next();
             
             System.out.println("");
+
+            //Loops through ASCII numbers of lowercase alphabet from 'a' to 'z'
             if (reply.equals("lower")){
                 for (int i = 97; i <= 122; i++) {
                     alphabet[index] = ("" + (char) i);
                     index++;
                 }
             }
+            //Turns the above input into their uppercase letter variants
             else if (reply.equals("upper")) {
                 for (int i = 97; i <= 122; i++) {
                     alphabet[index] = ("" + (char) i).toUpperCase();
@@ -33,6 +38,9 @@ public class sprint{
             }
       
         String output = "";
+        
+            /*Prints out the alphabet - after every iteration it adds a letter 
+            to the output variable and prints it in a new line*/
 
         for(int i = 0; i<rows; i++){
             output += alphabet[i];
