a3bc082c81b1a70fe09c6868cc3d4a63eec47ca2
Aidan Holloway <interncptwex037@capaciti.org.za>
2020-06-18 17:45:36 +0200

    Finished Code of Program
    Finished Coding
    
    Co-Authored-By: Michael Munnik <zaenzoza@users.noreply.github.com>

--- Sprint 3/sprint.java
+++ Sprint 3/sprint.java
@@ -2,14 +2,42 @@ import java.util.Scanner;
 
 public class sprint{
     public static void main(String[] args) {
-    System.out.println("Enter the number of rows to display.");
+    System.out.println("Enter the number of rows to display. Enter a value between 1 and 26");
         Scanner input = new Scanner(System.in);
         
         int rows = input.nextInt();
+        if(rows<1 || rows>26){
+            System.out.println("Invalid value entered. Exiting program.");
+            System.exit(0);
+        }
+        System.out.println("");
 
-        Char[] alphabet = new String[]{
-            'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'
-    };
-        
+        String[] alphabet = new String[26];
+        int index = 0;
+
+            System.out.println("Would you like the output to be in lowercase or uppercase letters? Please enter 'lower' or 'upper'.");
+            String reply = input.next();
+            
+            System.out.println("");
+            if (reply.equals("lower")){
+                for (int i = 97; i <= 122; i++) {
+                    alphabet[index] = ("" + (char) i);
+                    index++;
+                }
+            }
+            else if (reply.equals("upper")) {
+                for (int i = 97; i <= 122; i++) {
+                    alphabet[index] = ("" + (char) i).toUpperCase();
+                    index++;
+                }
+            }
+      
+        String output = "";
+
+        for(int i = 0; i<rows; i++){
+            output += alphabet[i];
+            System.out.println(output);
+        } 
+        input.close();
     }
 }
