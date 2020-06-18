6b8f0883cc659a674b25da32215e404d7a794147
Aidan Holloway <interncptwex037@capaciti.org.za>
2020-06-18 16:58:20 +0200

    Update sprint.java
    Added the Scanner and int variable to store user input

--- Sprint 3/sprint.java
+++ Sprint 3/sprint.java
@@ -1,8 +1,15 @@
+import java.util.Scanner;
+
 public class sprint{
     public static void main(String[] args) {
-    Char[] alphabet = new String[]{
+    System.out.println("Enter the number of rows to display.");
+        Scanner input = new Scanner(System.in);
+        
+        int rows = input.nextInt();
+
+        Char[] alphabet = new String[]{
             'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'
     };
-    
+        
     }
 }
